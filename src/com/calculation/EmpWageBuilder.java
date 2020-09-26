c7330655ab5773f5c89e7e736479442bc94297ae
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-26 19:03:21 +0530

    [Umesh3103]Added: calculating daily wage

--- src/com/calculation/EmpWageBuilder.java
+++ src/com/calculation/EmpWageBuilder.java
@@ -4,12 +4,20 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
 		int IS_FULL_TIME =1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
 		double empCheck =Math.floor(Math.random() *10) %2;
 		if(empCheck == IS_FULL_TIME){
+				empHrs=8;
 				System.out.println("Employee is Present");
 		}
-		else
+		else{
+			empHrs=0;
 			System.out.println("Employee is Absent");
+		}	
+		empWage=empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " +empWage);
 	
 	}
 }
