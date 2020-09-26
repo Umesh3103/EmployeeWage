80bed45b701bb16979a606095bdae4ff1964cb62
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-26 20:47:14 +0530

    [Umesh3103]Added: calculating wage for multiple companies

--- src/com/calculation/EmpWageBuilder.java
+++ src/com/calculation/EmpWageBuilder.java
@@ -3,17 +3,15 @@ package com.calculation;
 public class EmpWageBuilder {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;	
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
 	
-	public static int computeEmpWage() {
+	
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		System.out.println("Welcome to employee wage problem");
 		
 		int empHrs=0;
 		int empWage=0;
 		int totalEmpHrs=0,totalWorkingDays=0;
-		while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+		while(totalEmpHrs<= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays){
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() *10) %3;
 			switch(empCheck){
@@ -29,17 +27,18 @@ public class EmpWageBuilder {
 					System.out.println("Employee is Absent");
 			
 			}	
-			empWage=empHrs * EMP_RATE_PER_HOUR;
+			empWage=empHrs * empRatePerHour;
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " +totalWorkingDays + " Emp Hr: " +empHrs);
 			System.out.println("Emp Wage: " +empWage);
 		}
-		int totalEmpWage= totalEmpHrs * EMP_RATE_PER_HOUR;
+		int totalEmpWage= totalEmpHrs * empRatePerHour;
 		System.out.println("Total Emp Wage: " +totalEmpWage);
 		return totalEmpWage;
 	
 	}
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("DMart", 20, 2, 10);
+		computeEmpWage("Reliance", 10, 4, 20);
 	}
 }
