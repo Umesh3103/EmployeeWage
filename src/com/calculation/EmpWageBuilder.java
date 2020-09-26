8a365a016e79b2d939af7bf2056591c4969f2845
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-26 19:38:30 +0530

    [Umesh3103]Added: wage calculating for max hours in a month and max days in a month

--- src/com/calculation/EmpWageBuilder.java
+++ src/com/calculation/EmpWageBuilder.java
@@ -5,13 +5,16 @@ public class EmpWageBuilder {
 	public static final int IS_PART_TIME=2;	
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
 		
 		int empHrs=0;
-		int empWage=0,totalEmpWage=0;
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++){
+		int empWage=0;
+		int totalEmpHrs=0,totalWorkingDays=0;
+		while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() *10) %3;
 			switch(empCheck){
 				case IS_FULL_TIME:
@@ -27,9 +30,11 @@ public class EmpWageBuilder {
 			
 			}	
 			empWage=empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " +totalWorkingDays + " Emp Hr: " +empHrs);
 			System.out.println("Emp Wage: " +empWage);
 		}
+		int totalEmpWage= totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " +totalEmpWage);
 	
 	}
